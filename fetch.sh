72db6c6c08c0734daccc12215b365e47ee2ee186
ssshooter <472356884@qq.com>
2020-09-29 21:06:59 +0800

    修改依赖获取方式

---
+++ fetch.sh
@@ -0,0 +1,3 @@
+curl -o lib/regenerator-runtime.js https://cdn.jsdelivr.net/npm/regenerator-runtime
+curl -o lib/MindElixir.js https://cdn.jsdelivr.net/npm/mind-elixir/dist/MindElixir.js
+curl -o lib/painter.js https://cdn.jsdelivr.net/npm/mind-elixir/dist/painter.js
